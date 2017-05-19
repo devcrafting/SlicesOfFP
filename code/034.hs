977711f9f8d0fe7760e1f4520d39411812599bf6
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-05-19 22:19:56 +0200

    Code already passing

--- code/034.hs
+++ code/034.hs
@@ -1,8 +1,9 @@
 -- 034.hs
 import Test.Hspec
+import PokerHands
 
-main = hspec $ do
-    describe "cards" $ do
-        it "should collect cards from a string" $ do
+main = hspec $ 
+    describe "cards" $ 
+        it "should collect cards from a string" $
             cards "8d Ah Qc"  `shouldBe`
                  [(Eight,Diamonds),(Ace,Hearts),(Queen,Clubs)]
